ad6cda40846b6435cf479624e59204d0957b97a3
Lucas Petitjean <28061629+MushAsterion@users.noreply.github.com>
2025-09-21 00:51:37 +0100

    Defined connector types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
+import type { TableDefinition, TableColumnDefinition } from './schemas';
+export { TableDefinition, TableColumnDefinition };
+
+import { Connector } from './connector';
+export { Connector };
+
 import { SchemaFX, type SchemaFXOptions } from './core';
 export { SchemaFXOptions };
 export default SchemaFX;
