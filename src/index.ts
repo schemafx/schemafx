63619cf80901de1af2392b8d060840c6e6b48c68
Lucas Petitjean <28061629+MushAsterion@users.noreply.github.com>
2025-09-27 13:04:02 +0100

    Flat exports

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,11 @@
 import { type TableDefinition, type TableColumnDefinition, TableColumnType } from './schemas';
-export { TableDefinition, TableColumnDefinition };
+export { TableDefinition, TableColumnDefinition, TableColumnType };
 
 import { zodToTableColumns } from './utils/zodToTableColumns';
 export const utils = { zodToTableColumns };
 
 import { Connector, ConnectorAuthType, ConnectorAuthPropType } from './connector';
-export { Connector };
-
-export const Enums = {
-    TableColumnType,
-    ConnectorAuthType,
-    ConnectorAuthPropType
-};
+export { Connector, ConnectorAuthType, ConnectorAuthPropType };
 
 import { SchemaFX, type SchemaFXOptions } from './core';
 export { SchemaFXOptions };
