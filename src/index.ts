0f4151762c50169e1ecd530afb5580bfa32da33e
Lucas Petitjean <28061629+MushAsterion@users.noreply.github.com>
2025-11-29 00:29:26 +0000

    Added Fastify base

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('Hello World.');
+import fastify from 'fastify';
+import 'dotenv/config';
+
+const app = fastify({
+    logger: true
+});
+
+// Start
+try {
+    const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+
+    await app.listen({ port, host: '0.0.0.0' });
+    console.log(`\n🚀 Server listening at http://localhost:${port}/`);
+} catch (err) {
+    app.log.error(err);
+    process.exit(1);
+}
