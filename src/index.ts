70ce4c8e0512270d069d009d8933c94b112bed4b
Lucas Petitjean <28061629+MushAsterion@users.noreply.github.com>
2025-11-29 00:21:28 +0000

    Cleared project files

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,1 @@
-import {
-    type TableDefinition,
-    type TableColumnDefinition,
-    AuthPayloadKeys,
-    TableColumnType
-} from './schemas';
-export { TableDefinition, TableColumnDefinition, AuthPayloadKeys, TableColumnType };
-
-import { zodToTableColumns } from './utils/zodToTableColumns';
-export const utils = { zodToTableColumns };
-
-import { Connector, ConnectorAuthType, ConnectorAuthPropType } from './connector';
-export { Connector, ConnectorAuthType, ConnectorAuthPropType };
-
-import { SchemaFX, type SchemaFXOptions } from './core';
-export { SchemaFXOptions };
-export default SchemaFX;
+console.log('Hello World.');
