31b7846315551d968d803d2bd660f1027fb0a63b
Lucas Petitjean <28061629+MushAsterion@users.noreply.github.com>
2025-11-30 13:58:41 +0000

    Added base application security

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,10 @@ import {
 import 'dotenv/config';
 
 import fastifyCors from '@fastify/cors';
+import fastifyRateLimit from '@fastify/rate-limit';
+import fastifyHealthcheck from 'fastify-healthcheck';
+import fastifyHelmet from '@fastify/helmet';
+
 import connectorHandler from './connectors/connectorHandler.js';
 import MemoryConnector from './connectors/memoryConnector.js';
 
@@ -19,10 +23,33 @@ const app = fastify({
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
+app.register(fastifyHelmet);
+
 app.register(fastifyCors, {
     origin: true,
     methods: ['GET', 'POST', 'OPTIONS'],
     allowedHeaders: ['Content-Type']
+});
+
+app.register(fastifyRateLimit, {
+    max: 100,
+    timeWindow: '1 minute',
+    allowList: ['127.0.0.1'], // Internal bypass
+    errorResponseBuilder: (_, context) => ({
+        code: 429,
+        error: 'Too Many Requests',
+        message: `Rate limit exceeded. Retry in ${Math.round(context.ttl / 1000)}s`
+    })
+});
+
+app.register(fastifyHealthcheck);
+
+app.setErrorHandler((error, _, reply) => {
+    reply.log.error(error);
+    return reply.status(500).send({
+        code: 'Internal Server Error',
+        message: 'Unexpected error occurred.'
+    });
 });
 
 app.register(connectorHandler, {
